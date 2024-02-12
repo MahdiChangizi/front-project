4f6b66c4bbb70c1dc3d67468771b65042eaba3b1
mahdichangizi <mchangizi10@gmail.com>
2024-02-12 22:03:51 +0330

    add tailwind in project and create navbar responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      osWald: ['osWand', 'sans-serif'],
+      dmsans: ['Dm Sans', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
